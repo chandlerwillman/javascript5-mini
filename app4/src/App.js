7e04cef20154e22186171a5d4fd25b593d91f1fb
Chandler Willman <Chandler@Chandlers-MacBook-Pro-3.local>
2019-03-26 19:26:48 -0600

    Finished app4

--- app4/src/App.js
+++ app4/src/App.js
@@ -5,25 +5,37 @@ import axios from 'axios';
 class App extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       cars: []
     }
   }
 
-  getCars() {
+  getCars = () => {
     axios.get('https://joes-autos.herokuapp.com/api/vehicles')
-    .then(res => {
-      this.setState({
-        cars: res
-      })
-    })
+      .then(res => {
+        this.setState({
+          cars: res.data
+        });
+      }).catch(error => {
+        console.log(error);
+      });
   }
 
   render() {
+    const listOfCars = this.state.cars.map(car => {
+      return(
+        <div key={car.id}>
+          <h3>{car.year} {car.make} {car.model}</h3>
+          <p>${car.price}</p>
+        </div>
+      )
+    })
+    
     return (
       <div className="App">
         <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
+        {listOfCars}
       </div>
     );
   }
